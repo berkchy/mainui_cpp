a81e505cf1a4f838789cbfafa5364f81c1a47d77
pickle <pickle@local>
2026-09-14 00:09:46 +0300

    menus: simplify to Console, Multiplayer, Options, Quit

--- menus/Main.cpp
+++ menus/Main.cpp
@@ -201,27 +201,6 @@ void CMenuMain::_Init( void )
 		EngFuncs::KEY_SetDest( KEY_CONSOLE );
 	});
 
-	resumeGame.SetNameAndStatus( L( "GameUI_GameMenu_ResumeGame" ), L( "StringsList_188" ) );
-	resumeGame.SetPicture( PC_RESUME_GAME );
-	resumeGame.iFlags |= QMF_NOTIFY;
-	resumeGame.onReleased = UI_CloseMenu;
-
-	disconnect.SetNameAndStatus( L( "GameUI_GameMenu_Disconnect" ), L( "Disconnect from server" ) );
-	disconnect.SetPicture( PC_DISCONNECT );
-	disconnect.iFlags |= QMF_NOTIFY;
-	disconnect.onReleased = VoidCb( &CMenuMain::DisconnectDialogCb );
-
-	newGame.SetNameAndStatus( L( "GameUI_NewGame" ), L( "StringsList_189" ) );
-	newGame.SetPicture( PC_NEW_GAME );
-	newGame.iFlags |= QMF_NOTIFY;
-	newGame.onReleased = UI_NewGame_Menu;
-
-	hazardCourse.SetNameAndStatus( L( "GameUI_TrainingRoom" ), L( "StringsList_190" ) );
-	hazardCourse.SetPicture( PC_HAZARD_COURSE );
-	hazardCourse.iFlags |= QMF_NOTIFY;
-	hazardCourse.onReleasedClActive = VoidCb( &CMenuMain::HazardCourseDialogCb );
-	hazardCourse.onReleased = VoidCb( &CMenuMain::HazardCourseCb );
-
 	multiPlayer.SetNameAndStatus( L( "GameUI_Multiplayer" ), L( "StringsList_198" ) );
 	multiPlayer.SetPicture( PC_MULTIPLAYER );
 	multiPlayer.iFlags |= QMF_NOTIFY;
@@ -231,20 +210,6 @@ void CMenuMain::_Init( void )
 	configuration.SetPicture( PC_CONFIG );
 	configuration.iFlags |= QMF_NOTIFY;
 	configuration.onReleased = UI_Options_Menu;
-
-	saveRestore.iFlags |= QMF_NOTIFY;
-	saveRestore.onReleasedClActive = UI_SaveLoad_Menu;
-	saveRestore.onReleased = UI_LoadGame_Menu;
-
-	customGame.SetNameAndStatus( L( "GameUI_ChangeGame" ), L( "StringsList_530" ) );
-	customGame.SetPicture( PC_CUSTOM_GAME );
-	customGame.iFlags |= QMF_NOTIFY;
-	customGame.onReleased = UI_CustomGame_Menu;
-
-	previews.SetNameAndStatus( L( "Previews" ), L( "StringsList_400" ) );
-	previews.SetPicture( PC_PREVIEWS );
-	previews.iFlags |= QMF_NOTIFY;
-	SET_EVENT( previews.onReleased, EngFuncs::ShellExecute( MenuStrings[ IDS_MEDIA_PREVIEWURL ], NULL, false ) );
 
 	quit.SetNameAndStatus( L( "GameUI_GameMenu_Quit" ), L( "GameUI_QuitConfirmationText" ) );
 	quit.SetPicture( PC_QUIT );
@@ -261,55 +226,13 @@ void CMenuMain::_Init( void )
 	minimizeBtn.eFocusAnimation = QM_HIGHLIGHTIFFOCUS;
 	minimizeBtn.onReleased.SetCommand( FALSE, "minimize\n" );
 
-	if ( gMenu.m_gameinfo.gamemode == GAME_MULTIPLAYER_ONLY || gMenu.m_gameinfo.startmap[0] == 0 )
-		newGame.SetGrayed( true );
-
-	if ( gMenu.m_gameinfo.gamemode == GAME_SINGLEPLAYER_ONLY )
-		multiPlayer.SetGrayed( true );
-
-	if ( gMenu.m_gameinfo.gamemode == GAME_MULTIPLAYER_ONLY )
-	{
-		saveRestore.SetGrayed( true );
-		hazardCourse.SetGrayed( true );
-	}
-
-	// too short execute string - not a real command
-	if( strlen( MenuStrings[IDS_MEDIA_PREVIEWURL] ) <= 3 )
-	{
-		previews.SetGrayed( true );
-	}
-
-	// server.dll needs for reading savefiles or startup newgame
-	if( !EngFuncs::CheckGameDll( ))
-	{
-		saveRestore.SetGrayed( true );
-		hazardCourse.SetGrayed( true );
-		newGame.SetGrayed( true );
-	}
-
 	dialog.Link( this );
 
 	AddItem( background );
 	AddItem( banner );
-
-	if ( gpGlobals->developer )
-		AddItem( console );
-
-	AddItem( disconnect );
-	AddItem( resumeGame );
-	AddItem( newGame );
-
-	if ( bTrainMap )
-		AddItem( hazardCourse );
-
-	AddItem( saveRestore );
-	AddItem( configuration );
+	AddItem( console );
 	AddItem( multiPlayer );
-
-	if ( bCustomGame )
-		AddItem( customGame );
-
-	AddItem( previews );
+	AddItem( configuration );
 	AddItem( quit );
 	AddItem( minimizeBtn );
 	AddItem( quitButton );
@@ -324,57 +247,15 @@ void CMenuMain::VidInit( bool connected )
 {
 	CMenuPicButton::ClearButtonStack();
 
-	// statically positioned items
 	minimizeBtn.SetRect( uiStatic.width - 72, 13, 32, 32 );
 	quitButton.SetRect( uiStatic.width - 36, 13, 32, 32 );
-	disconnect.SetCoord( 72, 180 );
-	resumeGame.SetCoord( 72, 230 );
-	newGame.SetCoord( 72, 280 );
-	hazardCourse.SetCoord( 72, 330 );
-
-	bool isGameLoaded = EngFuncs::GetCvarFloat( "host_gameloaded" ) != 0.0f;
-	bool isSingle = gpGlobals->maxClients < 2;
-
-	if( isGameLoaded && isSingle )
-	{
-		saveRestore.SetNameAndStatus( L( "Save\\Load Game" ), L( "StringsList_192" ) );
-		saveRestore.SetPicture( PC_SAVE_LOAD_GAME );
-	}
-	else
-	{
-		saveRestore.SetNameAndStatus( L( "GameUI_LoadGame" ), L( "StringsList_191" ) );
-		saveRestore.SetPicture( PC_LOAD_GAME );
-	}
-
-	if( connected )
-	{
-		resumeGame.Show();
-		if( !isGameLoaded && !isSingle )
-		{
-			disconnect.Show();
-			console.pos.y = 130;
-		}
-		else
-		{
-			disconnect.Hide();
-			console.pos.y = 180;
-		}
-	}
-	else
-	{
-		resumeGame.Hide();
-		disconnect.Hide();
-		console.pos.y = 230;
-	}
 
 	console.pos.x = 72;
+	console.pos.y = 180;
 	console.CalcPosition();
-	saveRestore.SetCoord( 72, bTrainMap ? 380 : 330 );
-	configuration.SetCoord( 72, bTrainMap ? 430 : 380 );
-	multiPlayer.SetCoord( 72, bTrainMap ? 480 : 430 );
-	customGame.SetCoord( 72, bTrainMap ? 530 : 480 );
-	previews.SetCoord( 72, (bCustomGame) ? (bTrainMap ? 580 : 530) : (bTrainMap ? 530 : 480) );
-	quit.SetCoord( 72, (bCustomGame) ? (bTrainMap ? 630 : 580) : (bTrainMap ? 580 : 530));
+	multiPlayer.SetCoord( 72, 230 );
+	configuration.SetCoord( 72, 280 );
+	quit.SetCoord( 72, 330 );
 }
 
 void CMenuMain::_VidInit()
